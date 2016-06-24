b632efa65214d38ee01ac7ab2081aef648514597
Charles Lomboni <clomboni@mundipagg.com>
2016-06-24 13:04:49 -0300

    Atualiza exemplo Redis

--- RedisConnectionTest/RedisConnectionTest/RedisConnectorHelper.cs
+++ RedisConnectionTest/RedisConnectionTest/RedisConnectorHelper.cs
@@ -1,30 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StackExchange.Redis;
+﻿using StackExchange.Redis;
+using System;
 
-namespace RedisConnectionTest
-{
-    class RedisConnectorHelper
-    {
-        static RedisConnectorHelper()
-        {
-            lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
-            {
-                return ConnectionMultiplexer.Connect("localhost");
-            });
-        }
+namespace RedisConnectionTest {
 
-        private static Lazy<ConnectionMultiplexer> lazyConnection;
+    internal class RedisConnectorHelper {
 
-        public static ConnectionMultiplexer Connection
-        {
-            get
-            {
-                return lazyConnection.Value;
-            }
+        // Explicação e implementação do padrão Singleton
+        // http://csharpindepth.com/articles/general/singleton.aspx
+
+        // Configurando uma conexão segura para o Redis
+        // http://gigi.nullneuron.net/gigilabs/setting-up-a-connection-with-stackexchange-redis/
+        // ConfigurationOptions explicações
+        // https://github.com/StackExchange/StackExchange.Redis/blob/master/Docs/Configuration.md
+        private static Lazy<ConfigurationOptions> configOptions = new Lazy<ConfigurationOptions>(() => {
+            var configOptions = new ConfigurationOptions();
+            configOptions.EndPoints.Add("localhost:6379");
+            configOptions.ClientName = "SafeRedisConnection";
+            configOptions.ConnectTimeout = 100000;
+            configOptions.SyncTimeout = 100000;
+            configOptions.AbortOnConnectFail = false;
+            return configOptions;
+        });
+
+        // Lazy Connection
+        // https://msdn.microsoft.com/en-us/library/dd997286(v=vs.110).aspx
+        private static Lazy<ConnectionMultiplexer> conn = new Lazy<ConnectionMultiplexer>(() =>
+        ConnectionMultiplexer.Connect(configOptions.Value));
+
+        public static ConnectionMultiplexer SafeConn {
+            get { return conn.Value; }
         }
     }
 }
---
+++ Serilog/Exemplo-Serilog/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Sinks.RollingFile;
+
+namespace Exemplo_Serilog {
+    class Program {
+        static void Main(string[] args) {
+
+            //// Uma ILogger é criado usando LoggerConfiguration
+            //var log = new LoggerConfiguration().WriteTo.ColoredConsole().CreateLogger();
+
+            //log.Information("Inicio do Serilog!");
+
+            //Log.Logger = log;
+            //Log.Information("O log global foi configurado");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.LiterateConsole()
+                .WriteTo.RollingFile("logs\\myapp-{Date}.txt")
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
+
+            int a = 10, b = 0;
+            try {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            } catch (Exception ex) {
+                Log.Error(ex, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
+            Console.ReadKey();
+        }
+    }
+}
